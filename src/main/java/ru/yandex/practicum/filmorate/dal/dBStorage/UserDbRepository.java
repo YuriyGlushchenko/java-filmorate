d1e61fa1f3b32aabb8972214f0137bef81f6e5fa
Yuriy Glushchenko <yuriygl86@gmail.com>
2025-11-14 14:47:02 +0300

    friendship tests

--- src/main/java/ru/yandex/practicum/filmorate/dal/dBStorage/UserDbRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/dBStorage/UserDbRepository.java
@@ -5,23 +5,23 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.dal.UserStorage;
-import ru.yandex.practicum.filmorate.exceptions.exceptions.NotFoundException;
-import ru.yandex.practicum.filmorate.model.FriendshipStatus;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
 
 @Repository("UserDbRepository")
 @Slf4j
 public class UserDbRepository extends BaseRepository<User> implements UserStorage {
     private static final String FIND_ALL_QUERY = "SELECT * FROM users";
+
     private static final String FIND_BY_ID_QUERY = "SELECT * FROM users WHERE user_id = ?";
+
     private static final String FIND_BY_LOGIN_OR_EMAIL_QUERY = "SELECT * FROM users WHERE login = ? OR email = ?";
+
     private static final String INSERT_QUERY = "INSERT INTO users(email, login, name, birthday) VALUES (?, ?, ?, ?)";
+
     private static final String UPDATE_QUERY = "UPDATE users SET email = ?, login = ?, name = ?, birthday = ? WHERE user_id = ?";
-    private static final String FIND_USER_FRIENDS_IDS = "SELECT friend_id FROM friendship WHERE user_id = ? AND status_id = ?";
 
     public UserDbRepository(JdbcTemplate jdbc, RowMapper<User> mapper) {
         super(jdbc, mapper);
@@ -34,14 +34,13 @@ public class UserDbRepository extends BaseRepository<User> implements UserStorag
 
     @Override
     public Optional<User> getUserById(int id) {
-        Optional<User> userOptional = findOne(FIND_BY_ID_QUERY, id);
-        userOptional.ifPresent(this::loadUserFriends); // меняем данные User ВНУТРИ Optional, потом его же и возвращаем
 
-        return userOptional;
+        return findOne(FIND_BY_ID_QUERY, id);
     }
 
     @Override
     public Optional<User> findDuplicateDataUser(String email, String login) {
+
         return findOne(FIND_BY_LOGIN_OR_EMAIL_QUERY, login, email);
     }
 
@@ -56,18 +55,12 @@ public class UserDbRepository extends BaseRepository<User> implements UserStorag
                 java.sql.Date.valueOf(user.getBirthday()));
 
         user.setId(id);
+
         return user;
     }
 
     @Override
     public User update(User user) {
-
-        Optional<User> userOptional = findOne(FIND_BY_ID_QUERY, user.getId());
-
-        if (userOptional.isEmpty()) {
-            log.debug("Пользователь с id={} не найден", user.getId());
-            throw new NotFoundException("Данные не обновлены. Пользователь с id=" + user.getId() + " не найден");
-        }
 
         update(
                 UPDATE_QUERY,
@@ -78,18 +71,6 @@ public class UserDbRepository extends BaseRepository<User> implements UserStorag
                 user.getId()
         );
         return user;
-    }
-
-    private void loadUserFriends(User user) {
-        List<Integer> friendIds = jdbc.query(
-                FIND_USER_FRIENDS_IDS,
-                (rs, rowNum) -> rs.getInt("friend_id"),
-                user.getId(),
-                FriendshipStatus.CONFIRMED.getStatusId()
-        );
-
-        user.getFriends().clear();
-        user.getFriends().addAll(friendIds);
     }
 
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.dal.FriendshipStorage;
 import ru.yandex.practicum.filmorate.dal.UserStorage;
 import ru.yandex.practicum.filmorate.dto.UserDTO;
 import ru.yandex.practicum.filmorate.exceptions.exceptions.DuplicatedDataException;
@@ -12,6 +13,7 @@ import ru.yandex.practicum.filmorate.mapper.UserMapper;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -19,12 +21,15 @@ import java.util.stream.Collectors;
 @Service
 public class UserService {
     private final UserStorage userRepository;
+    private final FriendshipStorage friendshipRepository;
 
     // Вместо @Qualifier и хардкода выбираем конкретную реализацию бинов в файле настроек. Используется SpEL.
     // *{} - Spring Expression Language, внутри @имя_бина, ${} - значение из application.yml.
     @Autowired
-    public UserService(@Value("#{@${filmorate-app.storage.user-repository}}") UserStorage userRepository) {
+    public UserService(@Value("#{@${filmorate-app.storage.user-repository}}") UserStorage userRepository,
+                       FriendshipStorage friendshipRepository) {
         this.userRepository = userRepository;
+        this.friendshipRepository = friendshipRepository;
     }
 
     public Collection<UserDTO> getAllUsers() {
@@ -54,11 +59,13 @@ public class UserService {
     }
 
     public User update(User user) {
+        userRepository.getUserById(user.getId())
+                .orElseThrow(() -> new NotFoundException("Данные не обновлены. Пользователь с id=" + user.getId() + " не найден"));
+
         Optional<User> alreadyExistUser = userRepository.findDuplicateDataUser(user.getEmail(), user.getLogin());
 
         if (alreadyExistUser.isPresent()) {
             if (alreadyExistUser.get().getId() != user.getId()) {
-                log.trace("Login: {} или Email: {} уже используются", user.getLogin(), user.getEmail());
                 throw new DuplicatedDataException("Такой Login или Email уже используется");
             }
         }
@@ -67,11 +74,17 @@ public class UserService {
     }
 
     public User getUserById(int id) {
-        return userRepository.getUserById(id)
-                .orElseThrow(() -> {
-                    log.debug("Пользователь с id={} не найден", id);
-                    return new NotFoundException("Пользователь с id = " + id + " не найден");
-                });
+        User user = userRepository.getUserById(id)
+                .orElseThrow(() -> new NotFoundException("Пользователь с id = " + id + " не найден"));
+
+        List<Integer> friendsIds = friendshipRepository.getUserFriends(id).stream()
+                .map(User::getId)
+                .toList();
+
+        user.getFriends().clear();
+        user.getFriends().addAll(friendsIds);
+
+        return user;
     }
 
 }
