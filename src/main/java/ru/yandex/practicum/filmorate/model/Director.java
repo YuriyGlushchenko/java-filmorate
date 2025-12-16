568026d74b5bd5a5b49f4a33765f732d2b60a1b6
User Oksana <oks321mih@yandex.ru>
2025-12-16 22:52:59 +0300

    Удалила лишний импорт (остался от аннотации @Pattern)

--- src/main/java/ru/yandex/practicum/filmorate/model/Director.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Director.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
