bc75179849cbf06aa47ce4a0e0c9be18338b60ae
Ivan Berezin <uivan2009@rambler.ru>
2025-12-17 08:40:46 +0300

    add feed final: after review

--- src/main/java/ru/yandex/practicum/filmorate/model/Feed.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Feed.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 import lombok.Builder;
@@ -12,23 +11,23 @@ import lombok.Data;
 public class Feed {
 
     @NotNull(message = "Идентификатор события не может отсутствовать.")
-    int eventId;
+    Integer eventId;
 
     @PastOrPresent(message = "Время события не может быть в будущем.")
     @NotNull(message = "Время события не может отсутствовать.")
-    long timestamp;
+    Long timestamp;
 
     @JsonProperty("eventType")
-    @NotBlank(message = "Тип события не может отсутствовать.")
+    @NotNull(message = "Тип события не может отсутствовать.")
     FeedType feedType;
 
     @JsonProperty("operation")
-    @NotBlank(message = "Операция события не может отсутствовать.")
+    @NotNull(message = "Операция события не может отсутствовать.")
     FeedOperation feedOperation;
 
     @NotNull(message = "Идентификатор пользователя не может отсутствовать.")
-    int userId;
+    Integer userId;
 
     @NotNull(message = "Идентификатор сущности с которой связано событие не может отсутствовать.")
-    int entityId;
+    Integer entityId;
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/FeedService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FeedService.java
@@ -1,23 +1,20 @@
 package ru.yandex.practicum.filmorate.service;
 
+import jakarta.validation.constraints.NotNull;
 import ru.yandex.practicum.filmorate.model.Feed;
 import ru.yandex.practicum.filmorate.dal.dBStorage.FeedDBRepository;
 
-import lombok.extern.slf4j.Slf4j;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.validation.annotation.Validated;
 
 import java.util.Collection;
 
-@Slf4j
-@Validated
 @RequiredArgsConstructor
 @Service
 public class FeedService {
     private final FeedDBRepository feedRepository;
 
-    public Collection<Feed> findFeeds(Integer userId) {
+    public Collection<Feed> findFeeds(@NotNull Integer userId) {
         return feedRepository.getAllFeedById(userId);
     }
 
